f39859d089e73db9dc78cf8366d76c7da7685be0
marcelStep <marcel.stepien@rub.de>
2015-07-20 18:26:04 +0200

    trajectory.cpp in main.cpp umbenannt

---
+++ main.cpp
@@ -0,0 +1,49 @@
+// Compile with "g++ -std=c++11 main.cpp"
+#pragma once
+
+#include <vector>
+#include <iostream>
+#include "trajectory.h"
+#include "discreteFrechet.h"
+#include "unorderedCase.h"
+#include "orderedCase.h"
+#include "CSV_IO.h"
+#include "restrictedCase.h"
+
+int main (int argc, char** argv) {
+
+	char delim = ',';
+	CSV_IO<2> reader;
+	//CSV_IO<3> reader;
+	
+	//reader.read("\Bsp_1.csv", delim);
+	reader.read("\Bsp_2.csv", delim);
+	//reader.read("\Bsp_3.csv", delim);
+	//reader.read("\Bsp_3D.csv", delim);
+
+	//reader.read("C:\\input\\bg_UTM_1.csv", delim);
+	//reader.read("C:\\input\\bg_UTM_2.csv", delim);
+
+
+	//DiscreteFrechet<2> un(reader.getData());
+	UnorderedCase<2> un(reader.getData());
+	//OrderedCase<2> un(reader.getData());
+	//RestrictedCase<2> un(reader.getData());
+
+	//UnorderedCase<3> un(reader.getData());
+	//OrderedCase<3> un(reader.getData());
+	//RestrictedCase<3> un(reader.getData());
+
+	un.run();
+	cout << "Pfad Vollstaendig" << endl;
+	un.printResultsCompletly();
+	cout << endl;
+	cout << "Pfad Ohne doppelte vorkommen" << endl;
+	un.printResults();
+
+	string pfad = "C:\\output\\";
+	double epsilen = un.getEpsilon();
+	reader.write(un.getMiddleCurve(), epsilen, pfad);
+
+	return 0;
+}
